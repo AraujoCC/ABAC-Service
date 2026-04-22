4b4b033d2a67d585d804002c9acfdde3ad43a117
AraujoCC <pietroisaias1@gmail.com>
2026-04-22 15:42:00 -0300

    first commit

---
+++ src/main/java/com/example/abac/AbacServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.abac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbacServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbacServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/abac/AbacServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbacServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
